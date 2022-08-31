df3898b22399cf6ed512963749eb43679e9bdcf4
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2022-08-31 08:52:09 +0300

    feat: copy MainPage

---
+++ src/pages/MainPage/MainPage.ts
@@ -0,0 +1,40 @@
+export class MainPage {
+
+  init = (): void => {
+    this.drawPage();
+  }
+
+  drawPage = (): void => {
+    const pageElement = document.querySelector('#page') as HTMLElement;
+    const mainEl = document.createElement("main");
+    mainEl.className = "main";
+    mainEl.innerHTML = `<div class="main__container">
+      <div class="main__item left-main">
+        <div class="left-main__top">
+          <h1 class="left-main__title">STUDY ENGLISH</h1>
+          <div class="left-main__text">
+            Нескучное онлайн-обучение английскому языку с помощью игр и интересных заданий в любое удобное для вас время
+          </div>
+          <button class="left-main__button btn btn--primery">Начать обучение</button>
+        </div>
+        <div class="left-main__bottom">
+          <div class="left-main__achievements-block">
+            <div class="achievements__item achievements__left">
+              <div class="achievements__title">28k+</div>
+              <div class="achievements__text">активных пользователей платформы</div>
+            </div>
+            <div class="achievements__item achievements__right">
+              <div class="achievements__title">5 лет</div>
+              <div class="achievements__text">успешно обучаем языку</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main__item right-main">
+        <img class="right-main__img-block" src="./main-img.svg">
+      </div>
+      </div>
+  `
+  pageElement.append(mainEl);
+  }
+}
